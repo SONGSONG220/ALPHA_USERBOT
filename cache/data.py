1e1c131783bda96256df3488a28f871f40e67d49
Song <satypaldhakad0@gmail.com>
2026-04-26 09:52:12 +0530

    Update data.py

--- cache/data.py
+++ cache/data.py
@@ -205,13 +205,13 @@ RAID = [
 ]
 
 
-GRP = "@WORLD_ALPHA"
+GRP = "@bothub13"
 
-GROUP = [-1002094067251]
+GROUP = [-1003715911333]
 
-VERIFIED_USERS = [6730956183]
+VERIFIED_USERS = [7812646657]
 
-Owners = "@ll_ALPHA_BABY_lll|@II_RAJA_BABU_II"
+Owners = "@usernametnot|@usernametnot"
 
 PORN = [
         "https://telegra.ph/file/9bcc076fd81dfe3feb291.mp4",
